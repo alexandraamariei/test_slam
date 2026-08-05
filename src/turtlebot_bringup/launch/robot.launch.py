c6085b2b8bc4217ede91aff01893748ccb705a5d
Amariei Alexandra <alexandraamariei@yahoo.com>
2026-08-05 11:22:03 +0300

    Rezolvat comenzi tasatatura si viteza roti

--- src/turtlebot_bringup/launch/robot.launch.py
+++ src/turtlebot_bringup/launch/robot.launch.py
@@ -8,19 +8,18 @@ from launch.conditions import IfCondition, UnlessCondition
 
 def generate_launch_description():
 
-    # 1. Declarăm parametrul magic: use_sim_time (True = Gazebo, False = Hardware Real)
+    # 1. Declarăm parametrul magic: use_sim_time (True = Gazebo, False =  Real)
     use_sim_time = LaunchConfiguration('use_sim_time')
     declare_use_sim_time_cmd = DeclareLaunchArgument(
         'use_sim_time',
         default_value='true',
-        description='Foloseste timpul din simulare daca esti in Gazebo'
+        description='Folosesc timpul din simulare daca sunt in Gazebo'
     )
 
-    # 2. Căutăm pachetul nostru de descriere (cel vechi, cu URDF-ul)
+    # 2. Găsesc pachetul cu descrierea robotului (URDF-ul)
     turtlebot_desc_dir = get_package_share_directory('turtlebot_description')
 
-    # 3. Definim ce se lansează DACA suntem in SIMULARE (use_sim_time = true)
-    # Aici chemăm fișierul tău existent sim.launch.py
+    # 3. Lansarea pentru Simulare
     gazebo_sim_launch = IncludeLaunchDescription(
         PythonLaunchDescriptionSource(
             os.path.join(turtlebot_desc_dir, 'launch', 'sim.launch.py')
@@ -28,17 +27,19 @@ def generate_launch_description():
         condition=IfCondition(use_sim_time)
     )
 
-    # 4. Definim ce se lansează DACA suntem pe HARDWARE REAL (use_sim_time = false)
-    # Deocamdată nu avem fișierele reale, așa că le lăsăm pregătite teoretic
-    # (Aici vom pune micro_ros_agent, driverul de Lidar real, etc.)
-    # real_hardware_launch = IncludeLaunchDescription(...) 
+    # 4. Lansarea pentru Hardware Real
+    # (Aici pun micro_ros_agent, driverul de Lidar real, etc.)
+    #     PythonLaunchDescriptionSource(
+    #         os.path.join(turtlebot_desc_dir, 'launch', 'real_hardware.launch.py')
+    #     ),
+    #     condition=UnlessCondition(use_sim_time)
+    # )
 
-    # 5. Împachetăm totul și trimitem către ROS 2
+    # 5. trimit către ROS 2
     ld = LaunchDescription()
     ld.add_action(declare_use_sim_time_cmd)
     ld.add_action(gazebo_sim_launch)
     
-    # Când vom avea hardware-ul:
-    # ld.add_action(real_hardware_launch)
+
 
     return ld
--- src/turtlebot_description/launch/sim.launch.py
+++ src/turtlebot_description/launch/sim.launch.py
@@ -8,37 +8,37 @@ import xacro
 
 def generate_launch_description():
     pkg_path = get_package_share_directory('turtlebot_description')
-    xacro_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro') # sau .urdf
-    
-    # Transformăm xacro în xml dacă este cazul
+    xacro_file = os.path.join(pkg_path, 'urdf', 'robot.urdf.xacro') 
+
+    # Transformăm xacro în xml 
     robot_description_config = xacro.process_file(xacro_file)
     robot_description = {'robot_description': robot_description_config.toxml()}
 
-    # 1. Nodul obligatoriu care publică base_link și restul pieselor
+    # 1. Nodul care publică base_link și restul pieselor
     node_robot_state_publisher = Node(
         package='robot_state_publisher',
         executable='robot_state_publisher',
         output='screen',
         parameters=[robot_description, {'use_sim_time': True}]
     )
-    # 3. Porniți Gazebo Harmonic
+    # 3. Gazebo (fsiier de lansare mare)
     gazebo = IncludeLaunchDescription(
         PythonLaunchDescriptionSource([os.path.join(
             get_package_share_directory('ros_gz_sim'), 'launch', 'gz_sim.launch.py')]),
         launch_arguments={'gz_args': '-r empty.sdf'}.items()
     )
 
-    # 4. Spawnați robotul în Gazebo
+    # 4. robotul în Gazebo
     spawn_entity = Node(
         package='ros_gz_sim',
         executable='create',
         arguments=['-topic', 'robot_description',
                    '-name', 'turtlebot_burger',
-                   '-z', '0.1'], # Îl lansăm puțin deasupra solului
+                   '-z', '0.1'], 
         output='screen'
     )
 
-  # 5. Puntea (Bridge) între ROS 2 și Gazebo
+  # 5. Bridge între ROS 2 și Gazebo
     bridge = Node(
             package='ros_gz_bridge',
             executable='parameter_bridge',
@@ -48,7 +48,6 @@ def generate_launch_description():
                 '/odom@nav_msgs/msg/Odometry[gz.msgs.Odometry',
                 '/scan@sensor_msgs/msg/LaserScan[gz.msgs.LaserScan',
                 '/tf@tf2_msgs/msg/TFMessage[gz.msgs.Pose_V',
-                # LINIA NOUĂ CARE REZOLVĂ ERORILE CU ROȘU PENTRU ROȚI:
                 '/joint_states@sensor_msgs/msg/JointState[gz.msgs.Model'
             ],
             output='screen'
